2e87dd3f2b32c8e30a245dc7eed45cd96120f032
granddaifuku <daifukuformac@gmail.com>
2022-03-31 16:37:32 +0900

    feat: impl base action

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/granddaifuku/slack_interactive_example
 
 go 1.17
 
-require (
-	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/slack-go/slack v0.10.2 // indirect
-)
+require github.com/slack-go/slack v0.10.2
+
+require github.com/gorilla/websocket v1.5.0 // indirect
--- main.go
+++ main.go
@@ -14,9 +14,9 @@ import (
 )
 
 const (
-	selectShopAction  = "select-shop"
 	selectDrinkAction = "select-drink"
 	confirmAction     = "confirm-to-buy"
+	okAction          = "ok"
 	cancelAction      = "cancel"
 )
 
@@ -26,6 +26,12 @@ func main() {
 		panic("SLACK_BOT_TOKEN is not set")
 	}
 	client := slack.New(token)
+
+	// お店の商品一覧
+	shops := map[string][]string{
+		"Starbucks": {"Caramel Frappucino", "Java Chip Frappuccino", "White Chocolate Mocha"},
+		"Veloce":    {"Blend Coffee", "Hot Chocolate", "Latte"},
+	}
 
 	http.HandleFunc("/slack/events", func(w http.ResponseWriter, r *http.Request) {
 		body, err := io.ReadAll(r.Body)
@@ -55,7 +61,7 @@ func main() {
 			}
 			w.Header().Set("Content-Type", "text/plain")
 			if _, err := w.Write([]byte(res.Challenge)); err != nil {
-				log.Panicln(err)
+				log.Println(err)
 				w.WriteHeader(http.StatusInternalServerError)
 
 				return
@@ -65,31 +71,38 @@ func main() {
 			switch event := innerEvent.Data.(type) {
 			case *slackevents.AppMentionEvent:
 				msg := strings.Split(event.Text, " ")
-				if len(msg) < 2 {
-					log.Panicln("Number of messages is not enough")
+				if len(msg) < 3 {
+					log.Println("Number of messages is not enough")
 					w.WriteHeader(http.StatusBadRequest)
 
 					return
 				}
 
 				command := msg[1]
+				shop := msg[2]
 				switch command {
 				case "buy":
-					// お店を選ぶ
-					text := slack.NewTextBlockObject(slack.PlainTextType, "Which store do you buy from?", false, false)
+					// 飲み物を選ぶ
+					text := slack.NewTextBlockObject(slack.PlainTextType, "What do you need?", false, false)
 					textSection := slack.NewSectionBlock(text, nil, nil)
 
-					// お店の選択肢を作成する
-					shops := []string{"Starbucks", "Veloce", "Doutor"}
-					options := make([]*slack.OptionBlockObject, 0, len(shops))
-					for _, drink := range shops {
+					// 飲み物の選択肢を作成する
+					drinks, ok := shops[shop]
+					// リストにないお店だった
+					if !ok {
+						log.Println("We cannot buy drink from " + shop)
+						w.WriteHeader(http.StatusBadRequest)
+					}
+					options := make([]*slack.OptionBlockObject, 0, len(drinks))
+					for _, drink := range drinks {
 						optionText := slack.NewTextBlockObject(slack.PlainTextType, drink, false, false)
 						options = append(options, slack.NewOptionBlockObject(drink, optionText, nil))
 					}
-					placeholder := slack.NewTextBlockObject(slack.PlainTextType, "Shop", false, false)
+					placeholder := slack.NewTextBlockObject(slack.PlainTextType, "Drink", false, false)
 					selectMenu := slack.NewOptionsSelectBlockElement(slack.OptTypeStatic, placeholder, "", options...)
 
-					actionBlock := slack.NewActionBlock(selectShopAction, selectMenu)
+					blockID := fmt.Sprintf("%s=%s", selectDrinkAction, shop)
+					actionBlock := slack.NewActionBlock(blockID, selectMenu)
 
 					blocks := slack.MsgOptionBlocks(textSection, actionBlock)
 
@@ -107,7 +120,7 @@ func main() {
 	http.HandleFunc("/slack/actions", func(w http.ResponseWriter, r *http.Request) {
 		payload := new(slack.InteractionCallback)
 		if err := json.Unmarshal([]byte(r.FormValue("payload")), payload); err != nil {
-			log.Panicln(err)
+			log.Println(err)
 			w.WriteHeader(http.StatusInternalServerError)
 
 			return
@@ -122,52 +135,30 @@ func main() {
 			}
 
 			action := payload.ActionCallback.BlockActions[0]
-			switch action.BlockID {
-			case selectShopAction:
-				// 飲み物を選ぶ
-				text := slack.NewTextBlockObject(slack.PlainTextType, "What do you need?", false, false)
-				textSection := slack.NewSectionBlock(text, nil, nil)
-
-				// 飲み物の選択肢を作成する
-				drinks := []string{"coffee", "latte", "juice"}
-				options := make([]*slack.OptionBlockObject, 0, len(drinks))
-				for _, drink := range drinks {
-					optionText := slack.NewTextBlockObject(slack.PlainTextType, drink, false, false)
-					options = append(options, slack.NewOptionBlockObject(drink, optionText, nil))
-				}
-				placeholder := slack.NewTextBlockObject(slack.PlainTextType, "Drink", false, false)
-				selectMenu := slack.NewOptionsSelectBlockElement(slack.OptTypeStatic, placeholder, "", options...)
-
-				actionBlock := slack.NewActionBlock(selectDrinkAction, selectMenu)
-
-				blocks := slack.MsgOptionBlocks(textSection, actionBlock)
-
-				replaceOriginal := slack.MsgOptionReplaceOriginal(payload.ResponseURL)
-				if _, _, _, err := client.SendMessage(payload.ResponseURL, replaceOriginal, blocks); err != nil {
-					log.Println(err)
-					w.WriteHeader(http.StatusInternalServerError)
-
-					return
-				}
+			blockID := action.BlockID
+			actions := strings.Split(blockID, "=") // = で block ID を分割する
+			id := actions[0]                       // actions[0] に ID が入っている
+			switch id {
 			case selectDrinkAction:
 				// 確認ボタンを送付する
 				drink := action.SelectedOption.Value
 
+				shop := actions[1] // actions[1] にお店が入っている
 				text := slack.NewTextBlockObject(slack.MarkdownType,
-					fmt.Sprintf("Can I buy `%s` from `%s` ?", drink, ""), false, false)
+					fmt.Sprintf("Can I buy `%s` from `%s` ?", drink, shop), false, false)
 				textSection := slack.NewSectionBlock(text, nil, nil)
 
 				// Okボタン
-				confirmButtonText := slack.NewTextBlockObject(slack.PlainTextType, "Ok", false, false)
-				confirmButton := slack.NewButtonBlockElement(confirmAction, drink, confirmButtonText)
-				confirmButton.WithStyle(slack.StylePrimary)
+				okButtonText := slack.NewTextBlockObject(slack.PlainTextType, "Ok", false, false)
+				okButton := slack.NewButtonBlockElement(okAction, drink, okButtonText)
+				okButton.WithStyle(slack.StylePrimary)
 
 				// キャンセルボタン
 				cancelButtonText := slack.NewTextBlockObject(slack.PlainTextType, "Cancel", false, false)
 				cancelButton := slack.NewButtonBlockElement(cancelAction, "cancel", cancelButtonText)
 				cancelButton.WithStyle(slack.StyleDanger)
 
-				actionBlock := slack.NewActionBlock(confirmAction, confirmButton, cancelButton)
+				actionBlock := slack.NewActionBlock(confirmAction, okButton, cancelButton)
 				blocks := slack.MsgOptionBlocks(textSection, actionBlock)
 
 				replaceOriginal := slack.MsgOptionReplaceOriginal(payload.ResponseURL)
@@ -178,7 +169,10 @@ func main() {
 					return
 				}
 			}
+		case okAction:
+			// 何か処理をする：ここでは購入の処理など
 		case cancelAction:
+			// 再度選択肢を提示する
 		}
 	})
 
